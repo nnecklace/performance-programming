e70c5b2a39ea058806946370c974976eaf9aac0f
Niclas Forsman <niclas.forsman@futurice.com>
2020-04-18 19:37:14 +0300

    cleanup

--- assignment-2/bitarray/BitArray.h
+++ assignment-2/bitarray/BitArray.h
@@ -1,5 +1,4 @@
-#include <cstddef>
-#include <cstdint>
+#include <iostream>
 
 typedef unsigned long long ull;
 
@@ -11,17 +10,23 @@ class BitArray {
         const size_t size;
     public:
         BitArray(ull n);
+        BitArray();
         ~BitArray();
         ull get(const ull nth) const;
         void set(ull nth, ull i);
-        ull check(const ull block);
+        ull check(const ull block); // TODO: Remove!
         ull sum (const ull i) const;
         void compact();
+        friend std::istream& operator>>(std::istream& input, const BitArray& b);
+        friend std::ostream& operator<<(std::ostream& output, const BitArray& b);
 };
 
 BitArray::BitArray(ull n) : size(((n+63)/64)+1)
 {
     sequence = new ull[size];
+    sum_table = new uint8_t[size];
+    std::fill(sequence, sequence+size, 0);
+    std::fill(sum_table, sum_table+size, 0);
 }
 
 BitArray::~BitArray()
@@ -32,26 +37,46 @@ BitArray::~BitArray()
 
 ull BitArray::get(ull nth) const
 {
-    return ((sequence[nth/64]>>(nth%64)) & 1);
+    return sequence[nth/64]>>(nth&63) & 1;
 }
 
 void BitArray::set(ull nth, ull i)
 {
-    sequence[nth/64] |= (i<<(nth%64));
+    sequence[nth/64] |= i<<(nth&63);
 }
 
 ull BitArray::sum(const ull nth) const
 {
-    return sum_table[(nth/64)]+__builtin_popcountl(sequence[nth/64]<<(64-(nth%64)));
+    // incase we try to access illegal bits
+    if (nth >= size) {
+        return sum_table[size-1];
+    }
+
+    if (nth < 63) {
+        return __builtin_popcountl(sequence[0]<<(63-nth));
+    }
+
+    // ull incase we want to search for the sum up to some insane number, like 69 000 000
+    ull block = nth>>6;
+    return sum_table[block-1]+__builtin_popcountl(sequence[block]<<(63-(nth&63)));
 }
 
 void BitArray::compact()
 {
-    sum_table = new uint8_t[size];
     sum_table[0] = __builtin_popcountl(sequence[0]);
-    for (int i = 1; i < size; ++i) {
+    for (size_t i = 1; i < size; ++i) {
         sum_table[i] = __builtin_popcountl(sequence[i])+sum_table[i-1];
     }
+}
+
+std::istream& operator>>(std::istream& input, const BitArray& b)
+{
+    return input;
+}
+
+std::ostream& operator<<(std::ostream& output, const BitArray& b)
+{
+    return output;
 }
 
 // TODO: Remove! Only used for debugging
--- assignment-2/bitarray/main.cpp
+++ assignment-2/bitarray/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include "BitArray.h"
 #include <vector>
 #include <random>
 #include <chrono>
+
+#include "BitArray.h"
 
 // TODO: Remove! Only used for debugging
 #include <bitset>
 
 using namespace std;
 
-int main() {
+void test1() {
     int n, m;
     vector<int> bits;
 
@@ -45,4 +46,49 @@ int main() {
 
     // Cleanup
     delete ba;
+}
+
+void test2() {
+    int n, m;
+    vector<int> bits;
+
+    cin >> n;
+
+    BitArray* ba = new BitArray(n);
+
+    cin >> m;
+
+    bits.resize(m);
+
+    mt19937 random_value(random_device{}());
+    uniform_int_distribution<int> hat(0,n);
+    for (int i = 0; i < m; ++i) {
+        bits[i] = hat(random_value);
+    }
+
+    for (size_t i = 0; i < bits.size(); ++i) {
+        ba->set(bits[i], 1);
+    }
+
+    ba->compact();
+
+    cout << "Sum up to 1 " << ba->sum(1) << endl;
+    cout << "Sum up to 5 " << ba->sum(5) << endl;
+    cout << "Sum up to 11 " << ba->sum(11) << endl;
+    cout << "Sum up to 20 " << ba->sum(20) << endl;
+    cout << "Sum up to 50 " << ba->sum(50) << endl;
+    cout << "Sum up to 64 " << ba->sum(64) << endl;
+    cout << "Sum up to 75 " << ba->sum(75) << endl;
+    cout << "Sum up to 100 " << ba->sum(100) << endl;
+    cout << "Sum up to 200 " << ba->sum(200) << endl;
+    cout << "Sum up to 256 " << ba->sum(256) << endl;
+    cout << "Sum up to 4096 " << ba->sum(4096) << endl;
+    cout << "Sum up to 1000000 " << ba->sum(1000000) << endl;
+
+    // Cleanup
+    delete ba;
+}
+
+int main() {
+    test1();
 }
